2ba6ece82c357e0b1881839cf2fd07710b49b719
Dipen Kumar <dipenbachani@gmail.com>
2025-04-05 21:11:29 -0700

    implement user signup endpoint and initialize user handler

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,11 +4,21 @@ import (
 	"log"
 
 	"github.com/dipenkumarr/go-next-chat/db"
+	"github.com/dipenkumarr/go-next-chat/internal/user"
+	"github.com/dipenkumarr/go-next-chat/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatal("Could not initialize database: ", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
+
 }
---
+++ server/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/dipenkumarr/go-next-chat/internal/user"
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler) {
+	r = gin.Default()
+
+	r.POST("/signup", userHandler.CreateUser)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
