d0fd0e45c3b96ebbe0b4993b4c81bc048a5a621c
Dipen Kumar <dipenbachani@gmail.com>
2025-04-05 20:49:22 -0700

    implement user creation functionality with password hashing and repository pattern

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module github.com/dipenkumarr/go-next-chat
 
 go 1.24.1
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.36.0
+)
---
+++ server/internal/user/user.go
@@ -0,0 +1,31 @@
+package user
+
+import "context"
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	// GetUserByEmail(ctx context.Context, email string) (*User, error)
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error)
+}
